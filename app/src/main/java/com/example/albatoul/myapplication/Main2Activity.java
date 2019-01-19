68c8b7f577638edcf0a8cc82e2a7f03c03fd4318
letsCodeProject <ree72721@gmail.com>
2019-01-20 00:37:37 +0300

    //reem

--- app/src/main/java/com/example/albatoul/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/albatoul/myapplication/Main2Activity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-// here a am
+//bvjsvbslbvslvbslbvslvbsvbslrbgrgvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvdb
 
 public class Main2Activity extends AppCompatActivity {
 
